597d6e2e9096490f342565c4832da592d11ff692
Victor Magalhães <victormagalhaessales@gmail.com>
2026-03-24 12:32:04 -0300

    Initializing project

---
+++ src/main/java/com/victor/labs/tms_mass_updates/TmsMassUpdatesApplication.java
@@ -0,0 +1,13 @@
+package com.victor.labs.tms_mass_updates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsMassUpdatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmsMassUpdatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victor/labs/tms_mass_updates/TmsMassUpdatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.labs.tms_mass_updates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsMassUpdatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
